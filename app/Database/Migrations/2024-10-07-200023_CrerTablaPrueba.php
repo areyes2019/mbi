fb3f18e47eadd5a427bba285daeebe5f17898ee4
Abdias Reyes <reyesabdias@gmail.com>
2024-10-07 14:02:37 -0600

    creamos nuestra primer migracion

---
+++ app/Database/Migrations/2024-10-07-200023_CrerTablaPrueba.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CrerTablaPrueba extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'nombre' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '100',
+            ],
+            'email' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '100',
+            ],
+            'created_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+            'updated_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+            'deleted_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('mib_prueba');
+    }
+
+    public function down()
+    {
+        //
+    }
+}
