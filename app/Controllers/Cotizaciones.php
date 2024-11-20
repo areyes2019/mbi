473f34dff60c401d8dd96dc8f594dce799bdfdfb
Abdias Reyes <reyesabdias@gmail.com>
2024-11-20 12:49:59 -0600

    metimos el contenido html en una vista

--- app/Controllers/Cotizaciones.php
+++ app/Controllers/Cotizaciones.php
@@ -519,72 +519,8 @@ class Cotizaciones extends BaseController
         $email->setTo($resultado_usuario[0]['correo']); // Cambia por el correo del destinatario
         $email->setSubject("Cotización QT-$id");
         // Definir el contenido HTML del correo
-		$htmlContent = "
-		<!DOCTYPE html>
-		<html lang='es'>
-		<head>
-		    <meta charset='UTF-8'>
-		    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-		    <title>Cotización Solicitada</title>
-		    <style>
-		        body {
-		            font-family: Arial, sans-serif;
-		            background-color: #f4f4f4;
-		            color: #333;
-		            margin: 0;
-		            padding: 0;
-		        }
-		        .container {
-		            width: 100%;
-		            max-width: 600px;
-		            margin: 20px auto;
-		            background-color: #fff;
-		            padding: 20px;
-		            border-radius: 8px;
-		            box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
-		        }
-		        h2 {
-		            color: #007BFF;
-		        }
-		        p {
-		            font-size: 14px;
-		            line-height: 1.6;
-		        }
-		        .footer {
-		            margin-top: 30px;
-		            font-size: 12px;
-		            color: #777;
-		            text-align: center;
-		        }
-		    </style>
-		</head>
-		<body>
-
-		<div class='container'>
-		    <h2>Cotización Solicitada</h2>
-
-		    <p>Estimado/a <strong>Cliente</strong>,</p>
-
-		    <p>Gracias por tu interés en nuestros productos/servicios. Adjunto a este correo encontrarás el archivo PDF con la cotización solicitada.</p>
-
-		    <p>Si tienes alguna duda o necesitas realizar alguna modificación, no dudes en contactarnos. Estaremos encantados de ayudarte.</p>
-
-		    <p>Detalles de nuestra empresa:</p>
-		    <ul>
-		        <li><strong>Nombre de la Empresa:</strong> Grupo MBI S.A de C.V</li>
-		        <li><strong>Dirección:</strong> Privada Reynosa 455, Irapuato Gto</li>
-		        <li><strong>Teléfono:</strong> 4652554</li>
-		        <li><strong>Correo electrónico:</strong> admin@grupombi.com</li>
-		        <li><strong>Website:</strong> www.grupombi.com.mx</li>
-		    </ul>
-
-		    <p>Quedamos atentos a tu respuesta. ¡Gracias por confiar en nosotros!</p>
-
-		</div>
-
-		</body>
-		</html>
-		";
+		
+		$htmlContent = view('email/enviar_cotizacion');
         $email->setMessage($htmlContent);
         $email->attach($filePath);
 
