7bad14cb06f3fd37f60a129297385a3193916e7d
Abdias Reyes <reyesabdias@gmail.com>
2025-01-18 09:39:21 -0600

    se cambia el panel principal a modo responsivo

--- app/Views/panel.php
+++ app/Views/panel.php
@@ -12,212 +12,101 @@
         <h1 class="h3 mb-0 text-gray-800">Panel Principal</h1>
         <button class="btn btn-primary rounded-0 btn-sm mt-2 btn-sm shadow-none" data-toggle="modal" data-target="#nuevo_reporte"><span class="bi bi-filetype-pdf"></span> Nuevo Reporte</button>
     </div>
-    <div class="row">
-        <div class="col-12">
-        <?php if (es_super_admin()): ?>
-        <nav>
-            <div class="nav nav-tabs" id="nav-tab" role="tablist">
-                <button class="nav-link active" id="nav-home-tab" data-toggle="tab" data-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">Lista General</button>
-                <button class="nav-link" id="nav-profile-tab" data-toggle="tab" data-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">Mis Tareas</button>
-            </div>
-        </nav>
-        <div class="tab-content" id="nav-tabContent">
-            <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
-                <table id="personal" class="table table-bordered" style="width:100%">
-                    <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>Clientess</th>
-                            <th>Elaborado por:</th>
-                            <th>Fecha</th>
-                            <th>Estado</th>
-                            <th>Tipo</th>
-                            <th>Atendido por:</th>
-                            <th>Acciones</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php foreach ($kardex_admin as $mis_tareas): ?>
-                        <tr>
-                            <td><?php echo $mis_tareas['id_kardex'] ?></td>
-                            <td><?php echo $mis_tareas['hospital'] ?></td>
-                            <td><?php echo $mis_tareas['generado_nombre'] ?></td>
-                            <td>
-                                <?php
-                                $fecha_original = $mis_tareas['created_at'];
-                                $fecha_formateada = date("d-m-Y", strtotime($fecha_original));
-                                echo $fecha_formateada;
-                                ?>
-                            </td>
-                            <td>
-                                <?php 
-                                $estatus = asignar_estatus($mis_tareas['estatus']);
-                                if ($estatus): ?>
-                                    <span class="badge <?php echo $estatus['estilo']." ".$estatus['icon']?> "> <?php echo $estatus['nombre'] ?></span> 
-                                <?php endif ?>
-            
-
-                            </td>
-                            <td>
-                                <span v-if="<?php echo $mis_tareas['tipo'] ?> == 1" class="badge badge-pill badge-custom-yellow "><?php echo $mis_tareas['tipo_txt'] ?></span>
-                                <span v-else-if="<?php echo $mis_tareas['tipo'] ?> == 2" class="badge badge-pill badge-custom-orange "><?php echo $mis_tareas['tipo_txt'] ?></span>
-                                <span v-else-if="<?php echo $mis_tareas['tipo'] ?> == 3" class="badge badge-pill badge-custom-blue "><?php echo $mis_tareas['tipo_txt'] ?></span>
-                                <span v-else-if="<?php echo $mis_tareas['tipo'] ?> == 4" class="badge badge-pill badge-custom-aqua "><?php echo $mis_tareas['tipo_txt'] ?></span>
-                            </td>
-                            <td><strong><?php echo $mis_tareas['destinatario_nombre'] ?></strong></td>
-                            <td>
-                                <?php if (esc(permisos($mis_tareas['estatus'],'panel_eliminar'))): ?>
-                                <!--  Eliminar -->
-                                <button class="btn btn-danger btn-sm shadow-none mr-1"><span class="bi bi-trash3"></span></button>
-                                <?php endif ?>
-                                <!--  Editar -->
-                                <button class="btn btn-primary btn-sm shadow-none mr-1" @click = "editar_kardex('<?php echo $mis_tareas['slug'] ?>')"><span class="bi bi-pencil"></span></button>
-                                <!--  Vista Rápida -->
-                                <button class="btn btn-success btn-sm shadow-none"@click.prevent="ver_doc('<?php echo $mis_tareas['slug'] ?>')" data-toggle="modal" data-target="#ver_doc"><span class="bi bi-eye"></span></button>
-
-                            </td>
-                        </tr>
-                        <?php endforeach ?>
-                        
-                    </tbody>
-                </table>
-            </div>
-            <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
-                <table id="personal_user" class="table table-bordered" style="width:100%">
-                    <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>Clientess</th>
-                            <th>Elaborado por:</th>
-                            <th>Fecha</th>
-                            <th>Estado</th>
-                            <th>Tipo</th>
-                            <th>En Atencion</th>
-                            <th>Acciones</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php foreach ($kardex_user as $tareas): ?>
-                        <tr>
-                            <td>
-                                <?php echo $tareas['id_kardex'] ?>
-                            </td>
-                            <td><?php echo $tareas['hospital'] ?></td>
-                            <td><?php echo $tareas['generado_por'] ?></td>
-                            <td>
-                                <?php
-                                $fecha_original = $tareas['created_at'];
-                                $fecha_formateada = date("d-m-Y", strtotime($fecha_original));
-                                echo $fecha_formateada;
-                                ?>
-                            </td>
-                            <td>
-                                <?php 
-                                $estatus = asignar_estatus($mis_tareas['estatus']);
-                                if ($estatus): ?>
-                                    <span class="badge <?php echo $estatus['estilo'] ?>"><?php echo $estatus['nombre'] ?></span> 
-                                <?php endif ?>
-                            </td>
-                            <td>No asignado</td>
-                            <td>Misma</td>
-                            <td>
-                                <div class="dropdown">
-                                    <a  href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                      <i class="bi bi-three-dots-vertical"></i>
-                                    </a>
-
-                                    <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                                      <a class="dropdown-item" href="#"><span class="bi bi-trash3"></span> Eliminar</a> 
-
-                                      <a class="dropdown-item" href="<?php echo base_url('kardex/'.$tareas['slug']);?>"><span class="bi bi-pencil"></span> Editar</a>
-                                      <a class="dropdown-item" href="#" @click.prevent="ver_doc('<?php echo $tareas['id_kardex'] ?>')">Ver</a>
-                                    </div>
-                                </div>
-                            </td>
-                        </tr>
-                        <?php endforeach ?>       
-                    </tbody>
-                </table>
-            </div>
-        </div>
-        <?php endif ?>
-
-        <?php if (!es_super_admin()): ?>
-        <h4><span class="bi bi-filetype-pdf"></span> Mis Tareas</h4>
-        <div class="card rounded-0">
-            <div class="card-body">
-                <table id="personal" class="table table-bordered" style="width:100%">
-                    <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>Clientes</th>
-                            <th>Elaborado por:</th>
-                            <th>Fecha</th>
-                            <th>Estado</th>
-                            <th>Tipo</th>
-                            <th>En Atencion</th>
-                            <th></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php foreach ($kardex_user as $mis_tareas): ?>
-                        <tr>
-                            <td><?php echo $mis_tareas['id_kardex'] ?></td>
-                            <td><?php echo $mis_tareas['hospital'] ?></td>
-                            <td><?php echo $mis_tareas['generado_nombre'] ?></td>
-                            <td>
-                                <?php
-                                $fecha_original = $mis_tareas['created_at'];
-                                $fecha_formateada = date("d-m-Y", strtotime($fecha_original));
-                                echo $fecha_formateada;
-                                ?>
-                            </td>
-                            <td>
-                            <?php 
-                            $estatus = asignar_estatus($mis_tareas['estatus']);
-                            if ($estatus): ?>
-                                <span class="badge <?php echo $estatus['estilo'] ?>"><?php echo $estatus['nombre'] ?></span> 
-                            <?php endif ?>
-                            </td>
-                            <td>
-                                <span v-if="<?php echo $mis_tareas['tipo'] ?> == 1" class="badge badge-pill badge-custom-yellow "><?php echo $mis_tareas['tipo_txt'] ?></span>
-                                <span v-else-if="<?php echo $mis_tareas['tipo'] ?> == 2" class="badge badge-pill badge-custom-orange "><?php echo $mis_tareas['tipo_txt'] ?></span>
-                                <span v-else-if="<?php echo $mis_tareas['tipo'] ?> == 3" class="badge badge-pill badge-custom-blue "><?php echo $mis_tareas['tipo_txt'] ?></span>
-                                <span v-else-if="<?php echo $mis_tareas['tipo'] ?> == 4" class="badge badge-pill badge-custom-aqua "><?php echo $mis_tareas['tipo_txt'] ?></span>
-                            </td>
-                            <td><?php echo $mis_tareas['destinatario_nombre'] ?></td>
-                            <td>
-                                <div class="dropdown">
-                                  <button class="btn btn-outline-dark dropdown-toggle btn-sm rounded-0" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    <i class="bi bi-three-dots-vertical"></i>
-                                  </button>
-                                  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                                    <!--  Eliminar -->
-                                    <?php if (esc(permisos($mis_tareas['estatus'],'panel_eliminar'))): ?> 
-                                    <a class="dropdown-item" href="#" @click.prevent = "eliminar_kardex('<?php echo $mis_tareas['id_kardex'] ?>')">Eliminar</a>
-                                    <?php endif ?>
-                                    
-                                    <!--  Editar-->
-                                    <a class="dropdown-item" href="#" @click = "editar_kardex('<?php echo $mis_tareas['slug'] ?>')">Editar</a>
-                                    <?php if (esc(permisos($mis_tareas['estatus'],'panel_editar'))): ?>
-                                    <?php endif ?>
-                                    <!--  -->
-                                    <a class="dropdown-item" href="#" @click.prevent="ver_doc('<?php echo $mis_tareas['id_kardex'] ?>')" data-toggle="modal" data-target="#ver_doc">Vista Rápida</a>
-                                    <a class="dropdown-item" href="#">Enviar</a>
-                                  </div>
-                                </div>
-                            </td>
-                        </tr>
-                        <?php endforeach ?>
-                        
-                    </tbody>
-                </table>
-            </div>
-        </div>
-        <?php endif ?>
-        </div>    
+    <div class="mobile">
+        <ul class="list-group">
+            <?php foreach ($kardex_admin as $mis_tareas): ?>
+            <li class="list-group-item">
+                <?php 
+                $estatus = asignar_estatus($mis_tareas['estatus']);
+                if ($estatus): ?>
+                    <span class="badge <?php echo $estatus['estilo']." ".$estatus['icon']?> "> <?php echo $estatus['nombre'] ?></span> 
+                <?php endif ?>
+                <br>
+                <?php
+                $fecha_original = $mis_tareas['created_at'];
+                $fecha_formateada = date("d-m-Y", strtotime($fecha_original));
+                echo $fecha_formateada;
+                ?>
+                <p class="m-0"><strong>Hospital</strong></p>
+                <?php echo $mis_tareas['hospital'] ?>
+                <p class="m-0"><strong>Generado por:</strong></p>
+                <?php echo $mis_tareas['generado_nombre'] ?>
+                <br>
+                <span v-if="<?php echo $mis_tareas['tipo'] ?> == 1" class="badge badge-pill badge-custom-yellow "><?php echo $mis_tareas['tipo_txt'] ?></span>
+                <span v-else-if="<?php echo $mis_tareas['tipo'] ?> == 2" class="badge badge-pill badge-custom-orange "><?php echo $mis_tareas['tipo_txt'] ?></span>
+                <span v-else-if="<?php echo $mis_tareas['tipo'] ?> == 3" class="badge badge-pill badge-custom-blue "><?php echo $mis_tareas['tipo_txt'] ?></span>
+                <span v-else-if="<?php echo $mis_tareas['tipo'] ?> == 4" class="badge badge-pill badge-custom-aqua "><?php echo $mis_tareas['tipo_txt'] ?></span>
+                <br>
+                <?php if (esc(permisos($mis_tareas['estatus'],'panel_eliminar'))): ?>
+                <!--  Eliminar -->
+                <button class="btn btn-danger btn-sm shadow-none mr-1"><span class="bi bi-trash3"></span></button>
+                <?php endif ?>
+                <!--  Editar -->
+                <button class="btn btn-primary btn-sm shadow-none mr-1" @click = "editar_kardex('<?php echo $mis_tareas['slug'] ?>')"><span class="bi bi-pencil"></span></button>
+                <!--  Vista Rápida -->
+                <button class="btn btn-success btn-sm shadow-none"@click.prevent="ver_doc('<?php echo $mis_tareas['slug'] ?>')" data-toggle="modal" data-target="#ver_doc"><span class="bi bi-eye"></span></button>
+            </li>
+            <?php endforeach ?>        
+        </ul>
     </div>
-    
+    <div class="main-pc">        
+        <table id="personal" class="table table-bordered" style="width:100%">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Clientess</th>
+                    <th>Elaborado por:</th>
+                    <th>Fecha</th>
+                    <th>Estado</th>
+                    <th>Tipo</th>
+                    <th>Atendido por:</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($kardex_admin as $mis_tareas): ?>
+                <tr>
+                    <td><?php echo $mis_tareas['id_kardex'] ?></td>
+                    <td><?php echo $mis_tareas['hospital'] ?></td>
+                    <td><?php echo $mis_tareas['generado_nombre'] ?></td>
+                    <td>
+                        <?php
+                        $fecha_original = $mis_tareas['created_at'];
+                        $fecha_formateada = date("d-m-Y", strtotime($fecha_original));
+                        echo $fecha_formateada;
+                        ?>
+                    </td>
+                    <td>
+                        <?php 
+                        $estatus = asignar_estatus($mis_tareas['estatus']);
+                        if ($estatus): ?>
+                            <span class="badge <?php echo $estatus['estilo']." ".$estatus['icon']?> "> <?php echo $estatus['nombre'] ?></span> 
+                        <?php endif ?>
+
+
+                    </td>
+                    <td>
+                        <span v-if="<?php echo $mis_tareas['tipo'] ?> == 1" class="badge badge-pill badge-custom-yellow "><?php echo $mis_tareas['tipo_txt'] ?></span>
+                        <span v-else-if="<?php echo $mis_tareas['tipo'] ?> == 2" class="badge badge-pill badge-custom-orange "><?php echo $mis_tareas['tipo_txt'] ?></span>
+                        <span v-else-if="<?php echo $mis_tareas['tipo'] ?> == 3" class="badge badge-pill badge-custom-blue "><?php echo $mis_tareas['tipo_txt'] ?></span>
+                        <span v-else-if="<?php echo $mis_tareas['tipo'] ?> == 4" class="badge badge-pill badge-custom-aqua "><?php echo $mis_tareas['tipo_txt'] ?></span>
+                    </td>
+                    <td><strong><?php echo $mis_tareas['destinatario_nombre'] ?></strong></td>
+                    <td>
+                        <?php if (esc(permisos($mis_tareas['estatus'],'panel_eliminar'))): ?>
+                        <!--  Eliminar -->
+                        <button class="btn btn-danger btn-sm shadow-none mr-1"><span class="bi bi-trash3"></span></button>
+                        <?php endif ?>
+                        <!--  Editar -->
+                        <button class="btn btn-primary btn-sm shadow-none mr-1" @click = "editar_kardex('<?php echo $mis_tareas['slug'] ?>')"><span class="bi bi-pencil"></span></button>
+                        <!--  Vista Rápida -->
+                        <button class="btn btn-success btn-sm shadow-none"@click.prevent="ver_doc('<?php echo $mis_tareas['slug'] ?>')" data-toggle="modal" data-target="#ver_doc"><span class="bi bi-eye"></span></button>
+                    </td>
+                </tr>
+                <?php endforeach ?>        
+            </tbody>
+        </table>
+    </div>
     <div class="modal fade" id="nuevo_reporte" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content rounded-0">
